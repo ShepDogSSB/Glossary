9f95c47a19335803eb6d781aa9848a6291c74a5a
ShepDogSSB <42821013+ShepDogSSB@users.noreply.github.com>
2018-11-17 16:00:05 -0500

    Creates home page and reads input from a file.

--- Glossary/src/MakeGlossary.java
+++ Glossary/src/MakeGlossary.java
@@ -4,9 +4,9 @@ import components.simplewriter.SimpleWriter;
 import components.simplewriter.SimpleWriter1L;
 
 /**
- * Put a short phrase describing the program here.
+ * This program creates a glossary from an input file.
  *
- * @author Put your name here
+ * @author Nicholas Shepard
  *
  */
 public final class MakeGlossary {
@@ -17,6 +17,10 @@ public final class MakeGlossary {
     private MakeGlossary() {
     }
 
+    public static void createHomePage(SimpleWriter index) {
+        index.print("<!DOCTYPE html>");
+    }
+
     /**
      * Main method.
      *
@@ -24,15 +28,32 @@ public final class MakeGlossary {
      *            the command line arguments
      */
     public static void main(String[] args) {
-        // Basic I/O Variables.
+        /*
+         * Basic I/O Variable Instantiation
+         */
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
 
-        // Close I/O Streams
+        /*
+         * Creates home page and makes it into a valid html page.
+         */
+
+        SimpleWriter index = new SimpleWriter1L("data/index.html");
+        createHomePage(index);
+
+        /*
+         * Reads in the text file to use as the input.
+         */
+
+//        out.println("Enter Location & Name of Input File: ");
+//        SimpleReader fileIn = new SimpleReader1L(in.nextLine());
+
+        /*
+         * Close I/O Streams.
+         */
         in.close();
         out.close();
 
-        // This is a test
     }
 
 }
